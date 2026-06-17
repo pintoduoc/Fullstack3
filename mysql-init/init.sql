c884e6a84976d74139402ecf97c31fa544b80b1b
benja pinto <be.pintor@duocuc.cl>
2026-06-17 17:27:25 -0400

    commit inicial
    se saco del repositorio usado para parcial 2

---
+++ mysql-init/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS db_alertas;
+CREATE DATABASE IF NOT EXISTS db_reportes;
+CREATE DATABASE IF NOT EXISTS db_usuarios;
+USE db_reportes;
+
+DELIMITER //
+CREATE PROCEDURE ObtenerReportesPorEstado()
+BEGIN
+SELECT estado, COUNT(*) as cantidad
+FROM reporte_incendio
+GROUP BY estado;
+END //
+DELIMITER ;
